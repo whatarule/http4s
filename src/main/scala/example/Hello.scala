a7277acbe716fbbc1e8ec6f15dd2560c371cc5e2
wk <wk@DESKTOP-UU6O4B7.localdomain>
2021-02-08 23:01:05 +0900

    http

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,7 +1,27 @@
 package example
 
+import cats.effect._, org.http4s._, org.http4s.dsl.io._, scala.concurrent.ExecutionContext.Implicits.global
+import cats.syntax.all._
+import org.http4s.server.blaze._
+import org.http4s.implicits._
+import org.http4s.server.Router
+
 object Hello extends Greeting with App {
   println(greeting)
+
+  implicit val cs: ContextShift[IO] = IO.contextShift(global)
+  implicit val timer: Timer[IO] = IO.timer(global)
+
+  val helloWorldService = HttpRoutes.of[IO] { case GET -> Root / "hello" / name =>
+    Ok(s"hello, $name")
+  }
+
+  val services = helloWorldService
+  val httpApp = Router("/" -> helloWorldService, "/api" -> services).orNotFound
+  val serverBuilder = BlazeServerBuilder[IO](global).bindHttp(8080, "localhost").withHttpApp(httpApp)
+
+  val fiber = serverBuilder.resource.use(_ => IO.never).start.unsafeRunSync()
+  //fiber.cancel.unsafeRunAsync(e => ())
 }
 
 trait Greeting {
