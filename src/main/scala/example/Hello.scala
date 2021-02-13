ac345ebbcb3b012568f94b43ce420119f2427fac
whatarule <whatarule@gmail.com>
2021-02-13 22:54:22 +0900

    tapir

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,36 +1,42 @@
 package example
 
 import cats.effect._
-import org.http4s.HttpRoutes
-import org.http4s.dsl.io._
-//import scala.concurrent.ExecutionContext.Implicits.global
-
 import cats.syntax.all._
+
+import org.http4s.{HttpApp, HttpRoutes}
+import org.http4s.dsl.io._
 import org.http4s.server.blaze._
 import org.http4s.implicits._
 import org.http4s.server.Router
 
+//import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.ExecutionContext.global
+
+import sttp.tapir._
+import sttp.tapir.server.http4s._
 
 object Hello extends IOApp {
   implicit val cs: ContextShift[IO] = IO.contextShift(global)
   //implicit val timer: Timer[IO] = IO.timer(global)
 
-  val helloWorldService = HttpRoutes.of[IO] { case GET -> Root / "hello" / name =>
-    Ok(s"hello, $name!")
-  }
+  //val helloService = HttpRoutes.of[IO] { case GET -> Root / "hello" / name =>
+  //  Ok(s"hello, $name!")
+  //}
+  //val services = helloService
+  //val httpApp = Router("/" -> helloService, "/api" -> services).orNotFound
 
-  val services = helloWorldService
-  val httpApp = Router("/" -> helloWorldService, "/api" -> services).orNotFound
-  val serverBuilder = BlazeServerBuilder[IO](global).bindHttp(8080, "localhost").withHttpApp(httpApp)
-
-  //val fiber = serverBuilder.resource.use(_ => IO.never).start.unsafeRunSync()
-  //fiber.cancel.unsafeRunAsync(e => ())
+  def helloLogic(name: String): IO[Either[Unit, String]] =
+    IO.pure(s"hello, $name!".asRight[Unit])
+  val helloEP: Endpoint[String, Unit, String, Any] =
+    endpoint.get.in("hello" / path[String]("name")).out(stringBody)
+  val helloRoute: HttpRoutes[IO] = Http4sServerInterpreter.toRoutes(helloEP)(helloLogic _)
+  val helloService: HttpApp[IO] = helloRoute orNotFound
 
   def run(args: List[String]): IO[ExitCode] =
     BlazeServerBuilder[IO](global)
       .bindHttp(8080, "localhost")
-      .withHttpApp(httpApp)
+      //.withHttpApp(httpApp)
+      .withHttpApp(helloService)
       .serve
       .compile
       .drain
