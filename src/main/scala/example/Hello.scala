876b844149cff87c3e25a31f59cf910dfca86e2b
whatarule <whatarule@gmail.com>
2021-02-13 17:20:38 +0900

    server: graceful shutdown

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,29 +1,38 @@
 package example
 
-import cats.effect._, org.http4s._, org.http4s.dsl.io._, scala.concurrent.ExecutionContext.Implicits.global
+import cats.effect._
+import org.http4s.HttpRoutes
+import org.http4s.dsl.io._
+//import scala.concurrent.ExecutionContext.Implicits.global
+
 import cats.syntax.all._
 import org.http4s.server.blaze._
 import org.http4s.implicits._
 import org.http4s.server.Router
 
-object Hello extends Greeting with App {
-  println(greeting)
+import scala.concurrent.ExecutionContext.global
 
+object Hello extends IOApp {
   implicit val cs: ContextShift[IO] = IO.contextShift(global)
-  implicit val timer: Timer[IO] = IO.timer(global)
+  //implicit val timer: Timer[IO] = IO.timer(global)
 
   val helloWorldService = HttpRoutes.of[IO] { case GET -> Root / "hello" / name =>
-    Ok(s"hello, $name")
+    Ok(s"hello, $name!")
   }
 
   val services = helloWorldService
   val httpApp = Router("/" -> helloWorldService, "/api" -> services).orNotFound
   val serverBuilder = BlazeServerBuilder[IO](global).bindHttp(8080, "localhost").withHttpApp(httpApp)
 
-  val fiber = serverBuilder.resource.use(_ => IO.never).start.unsafeRunSync()
+  //val fiber = serverBuilder.resource.use(_ => IO.never).start.unsafeRunSync()
   //fiber.cancel.unsafeRunAsync(e => ())
-}
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+  def run(args: List[String]): IO[ExitCode] =
+    BlazeServerBuilder[IO](global)
+      .bindHttp(8080, "localhost")
+      .withHttpApp(httpApp)
+      .serve
+      .compile
+      .drain
+      .as(ExitCode.Success)
 }
